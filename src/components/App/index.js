0affe3d04aadb325a57bdee521cd99448cd60639
barlasch_sd2081@CEACCS.NET <barlasch_sd2081@CEACCS.NET>
2019-11-21 10:14:18 +0800

    initial

---
+++ src/components/App/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ConversationList from '../ConversationList';
+import './app.css'
+export default function App() {
+    return (
+      <div className="App">
+         
+         <ConversationList />
+  
+           
+      </div>
+    );
+}
